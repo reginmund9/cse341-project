033224204923023e4e04555899d00a3d85f3aa60
reginmund9 <moronitaca@gmail.com>
2024-07-05 06:54:23 -0600

    w3y4

---
+++ controllers/home.js
@@ -0,0 +1,9 @@
+const name = { name: 'Ramon Vargas - Project 2' };
+
+const justHome = async (req, res) => {
+  //#swagger.tags=['Home']
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).json(name.name);
+};
+
+module.exports = { justHome };
