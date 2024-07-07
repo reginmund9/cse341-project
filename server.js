661b80d29d5ca9e96878c56d9aa57a23d55c0ef6
reginmund9 <moronitaca@gmail.com>
2024-07-06 22:39:39 -0600

    name

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ passport.deserializeUser((user, done) => {
 });
 
 app.get('/', (req, res) => {
-  res.send(req.session.user !== undefined ? `Logged in as Ramon ${req.session.user.displayName}` : 'Logged Out' )
+  res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : 'Logged Out' )
 });
 
 app.get('/github/callback', passport.authenticate('github', {
